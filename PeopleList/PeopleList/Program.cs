d84446c89ff1936a0a4350b0e488dfa49091f75c
FanTei <passmirgeb@gmail.com>
2021-04-01 22:39:17 +0300

    Added List

---
+++ PeopleList/PeopleList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            People people = new People("Oleg",15,"IT");
+            
+            people.PrintAllInfo();
+
+        }
+    }
+}
